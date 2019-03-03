81ef3f563bc9321ce802b2a68d79265bd560fa1f
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2019-03-03 17:02:55 +0200

    Create flatlist.py

---
+++ flatlist.py
@@ -0,0 +1,16 @@
+def flatlist(array):
+    """
+       flatlist turns a nested list with any depth of nesting,
+       taken as a parameter into a flat list.
+       Example: [[2]], [4, [5, 6, [6], 6, 6, 6], 7]] -> [2, 4, 5, 6, 6, 6, 6, 6, 7]
+       Developed by Dennis Lozinskyi, 03/03/2019
+    """
+    
+    result = [] 
+    for i in array:
+        if isinstance(i, list):
+            result += flat_list(i) 
+        else:
+            result.append(i)
+               
+    return result
